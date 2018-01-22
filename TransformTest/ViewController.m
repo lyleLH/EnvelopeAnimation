9ab17805fc8d29a4e7690979933a3d1fcb8bc8f9
刘浩 <v2top1lyle@gmail.com>
2018-01-22 10:14:07 +0800

    信封打开的动画,TODO:信纸抽出动画

--- TransformTest/ViewController.m
+++ TransformTest/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *bgView;
+@property (weak, nonatomic) IBOutlet UIView *leftView;
+@property (weak, nonatomic) IBOutlet UIView *rightView;
 
 @end
 
@@ -17,12 +20,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.bgView bringSubviewToFront:self.rightView];
+}
+
+- (IBAction)left:(id)sender {
+    
+//    [self transformView:self.leftView angle:-M_PI  durantion:2.0 anchorPoint:CGPointMake(0, 0.5)];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)right:(id)sender {
+    
+    [self transformView:self.rightView angle:M_PI  durantion:2.0 anchorPoint:CGPointMake(1, 0.5) completion:^(BOOL finished) {
+        [self transformView:self.leftView angle:-M_PI  durantion:2.0 anchorPoint:CGPointMake(0, 0.5) completion:^(BOOL finished) {
+            
+        }];
+    }];
+    
+}
+
+
+- (void)transformView:(UIView*)view angle:(CGFloat)angle durantion:(CFTimeInterval)duration anchorPoint:(CGPoint)point  completion:(void (^ __nullable)(BOOL finished))completion{
+    //    CATransform3D rotate = CATransform3DMakeRotation(- M_PI/6, 0, 1, 0);
+    
+    
+    //构造一个旋转矩阵
+    CATransform3D rotate = CATransform3DMakeRotation(angle, 0, 1, 0);
+    //构造一个透视矩阵
+    /*
+     其中：M34属性 控制透视效果
+     
+     正的值向下（左）方往里缩放 负的值上（右）方往里缩放
+     */
+    
+    CGFloat disZ = 500;
+    
+    CATransform3D scale = CATransform3DIdentity;
+    
+    scale.m34 =  point.x ==0 ?- 1.0f/disZ:1.0f/disZ;
+    
+    //最终的变换矩阵
+    CATransform3D transform = CATransform3DConcat(rotate, scale);
+
+    
+    [UIView animateWithDuration:1.5 animations:^{
+         view.layer.transform = transform;
+    } completion:^(BOOL finished) {
+        completion(finished);
+    }];
+}
+
+
+- (IBAction)both:(id)sender {
+    
+    
 }
 
 
